d2d4afe4ea32141d91ac96a222e9d73ab76300b0
lucjones02 <luc.xav.jones@gmail.com>
2025-10-31 21:22:09 +0000

    implemented my vector and added some tests

--- include/vector.hpp
+++ include/vector.hpp
@@ -1,63 +1,421 @@
 #include <cstddef>
+#include <stdexcept>
+#include <utility>
+#include <vector>
+
 
 namespace my_std
 {
 
 template <typename T>
-struct my_vector 
+struct vector 
 {
-    T* _data_begin;
-    T* _data_end;
-    T* _alloc_end;
+    T* _data_begin {nullptr};
+    T* _data_end {nullptr};
+    T* _alloc_end {nullptr};
+
+    vector() = default;
+
+    explicit vector(std::size_t count)
+    {
+        std::size_t alloc_size = std::min(count, MAX_SIZE);
+        allocate(alloc_size);
+
+        while (_data_end != _alloc_end)
+        {
+            // *(_data_end) = std::move(T()); // TODO: I think this requires T to be default constructable
+            (_data_end) = new (_data_end) T();
+            ++_data_end;
+        }
+    }
+
+    explicit vector(std::size_t count, const T& value)
+    {
+        std::size_t alloc_size = std::min(count, MAX_SIZE);
+        allocate(alloc_size);
+
+        while (_data_end != _alloc_end)
+        {
+            *(_data_end) = value;
+            ++_data_end;
+        }
+    }
+    
+    ~vector()
+    {
+        deallocate();
+    }
+
+private:
+    // only allocates count * sizeof(T) raw bytes
+    void allocate(std::size_t alloc_size)
+    {
+        if (alloc_size == 0)
+        {
+            _data_begin = nullptr;
+            _data_end   = nullptr;
+            _alloc_end  = nullptr;
+            return;
+        }
+
+        // using operator new here because it allows me to only allocate raw bytes
+        // so constructor calls are kept for later
+        _data_begin = static_cast<T*>(::operator new(alloc_size * sizeof(T)));
+        _data_end   = _data_begin;
+        _alloc_end  = _data_begin + alloc_size; 
+    }
+
+    // TODO: alignment delete thing in the specs
+    void deallocate(T* data_begin, T* data_end)
+    {
+        if (data_begin != nullptr)
+        {
+            // reverse order from construction
+            T* it = data_end;
+            while (it != data_begin)
+            {
+                (--it)->~T();
+            }
+
+            // operator delete does not call destructor as used with operator new
+            // so all good here no double free
+            ::operator delete(data_begin);
+        }
+    }
+
+    void deallocate()
+    {
+        deallocate(_data_begin, _data_end);
+
+        _data_begin = nullptr;
+        _data_end   = nullptr;
+        _alloc_end  = nullptr;
+    }
+
+    std::size_t next_cap(const std::size_t cap) const
+    {
+        return 2 * cap;
+    }
+
+    static constexpr std::size_t MAX_SIZE = 1 << 12;
 
 
-    const T& at() const;
-    T& at();
+public:
+    const T& at(std::size_t pos) const
+    {
+        if (pos > _data_end - _data_begin) throw std::out_of_range("my_std::vector::at - ");
+        return *(_data_begin + pos);
+    }
 
-    const T& operator[]() const;
-    T& operator[]();
+    T& at(std::size_t pos)
+    {
+        return const_cast<T&>(std::as_const(*this).at(pos));
+    }
 
-    const T& front() const;
-    T& front();
 
-    const T& back() const;
-    T& back();
+    const T& operator[](std::size_t pos) const
+    {
+        return *(_data_begin + pos);
+    }
 
-    const T* begin() const;
-    T* begin();
+    T& operator[](std::size_t pos)
+    {
+        return const_cast<T&>(std::as_const(*this)[pos]);
+    }
+    
 
-    const T* end() const;
-    T* end();
+    const T& front() const
+    {
+        return *(_data_begin);
+    }
 
-    bool empty() const;
-    std::size_t size() const;
-    std::size_t max_size() const;
+    T& front()
+    {
+        return const_cast<T&>(std::as_const(*this).front());
+    }
+     
 
-    void reserve(std::size_t new_cap);
-    std::size_t capacity() const;
-    void shrink_to_fit();
+    const T& back() const
+    {
+        return *(_data_end - 1);
+    }
 
-    void clear();
+    T& back()
+    {
+        return const_cast<T&>(std::as_const(*this).back());
+    }
 
-    void insert(const T* pos, const T& value);
-    void insert(const T* pos, T&& value);
-    void insert(const T* pos, std::size_t count, const T& value);
 
-    T* erase(T* pos);
-    T* erase(const T* pos);
-    T* erase(T* first, T* last);
-    T* erase(const T* first, const T* last);
+    const T* begin() const
+    {
+        return _data_begin;
+    }
 
-    void push_back(const T& value);
-    void push_back(T&& value);
+    T* begin()
+    {
+        return const_cast<T*>(std::as_const(*this).begin());
+    }
 
-    void pop_back();
 
-    void resize(std::size_t count);
-    void resize(std::size_t count, const T& value);
+    const T* end() const
+    {
+        return _data_end;
+    }
 
-    void swap(my_vector& other) noexcept;
+    T* end()
+    {
+        return const_cast<T*>(std::as_const(*this).end());
+    }
 
+
+    std::size_t size() const
+    {
+        return (_data_end - _data_begin);
+    }
+
+    std::size_t max_size() const
+    {
+        return MAX_SIZE * sizeof(T);
+    }
+
+    bool empty() const
+    {
+        return this->size() == 0;
+    }
+
+
+    void reserve(std::size_t new_cap)
+    {
+        if (new_cap <= this->capacity()) return;
+
+        std::size_t old_size = this->size();
+        T* previous_start = _data_begin;
+
+        std::size_t alloc_size = std::min(new_cap, MAX_SIZE);
+        allocate(alloc_size);
+
+        if (previous_start == nullptr) return;
+
+        std::size_t pos = 0;
+        while (pos < old_size)
+        {
+            _data_end = new (_data_end) T(std::move(*(previous_start + pos)));
+            // *(_data_end) = std::move(*(previous_start + pos));
+            _data_end++;
+            pos++;
+        }
+
+        deallocate(previous_start, previous_start + old_size);
+    }
+
+    std::size_t capacity() const
+    {
+        return _alloc_end - _data_begin;
+    }
+
+    void shrink_to_fit()
+    {
+        std::size_t old_size = this->size();
+        T* previous_start = _data_begin;
+
+        allocate(this->size());
+        
+        if (previous_start == nullptr) return;
+
+        std::size_t pos = 0;
+        while (pos < old_size)
+        {
+            *(_data_end++) = std::move(*(previous_start + pos++));
+        }
+
+        delete previous_start;
+    }
+
+    void clear()
+    {
+        std::size_t cap = this->capacity();
+
+        deallocate();
+        allocate(cap);
+    }
+
+private:
+
+    template<typename Arg>
+    T* __insert_impl(const T* pos, std::size_t count, Arg&& value)
+    {
+        //TODO: this will leak memory if a new statement throws an error
+        if (count == 0) return const_cast<T*>(pos);
+
+        if (pos > _data_end || pos < _data_begin)
+        {
+            throw std::out_of_range("insert");
+        }
+
+        // resize if necessary
+        // we treat both of these cases seperately instead of calling resize if needed and then inserting
+        // because I can avoid moving over all the elements just to have to move more right after
+        if (this->size() + count > this->capacity())
+        {
+            T* _prev_begin = this->begin();
+            T* _prev_end   = this->end();
+
+            std::size_t alloc_size = std::min(next_cap(this->capacity()), MAX_SIZE);
+            allocate(alloc_size);
+
+            T* it  = _prev_begin;
+            while (it != pos)
+            {
+                new (_data_end++) T(std::move(*(it++)));
+            }
+
+            T* insertion_pos = _data_end;
+
+            while (count)
+            {
+                // we use forward here because considering how we call this function, we are guaranteed an lvalue ref here
+                // if count is > 1 so we will never move something twice
+                new (_data_end++) T(std::forward<Arg>(value));
+                count--;
+            }
+
+            while (it != _prev_end)
+            {
+                new (_data_end++) T(std::move(*(it++)));
+            }
+
+            deallocate(_prev_begin, _prev_end);
+
+            return insertion_pos;
+        }
+        else
+        {
+            T* new_it = _data_end + count;
+            T* old_it = _data_end;
+            while (old_it != pos)
+            {
+                new(--new_it) T(std::move(*(--old_it)));
+            }
+
+            T* it = const_cast<T*>(pos);
+            while (it < const_cast<T*>(pos) + count)
+            {
+                // we use forward here to avoid compilation errors but considering how we
+                // call this function, we are guaranteed an lvalue ref here
+                new ((it++)) T(std::forward<Arg>(value));
+                _data_end++;
+            }
+
+            return const_cast<T*>(pos);
+        }
+    }
+
+public:
+
+    [[maybe_unused]] T* insert(const T* pos, const T& value)
+    {
+        return __insert_impl(pos, 1, value);
+    }
+
+    [[maybe_unused]] T* insert(const T* pos, T&& value)
+    {
+        return __insert_impl(pos, 1, std::move(value));
+    }
+
+    [[maybe_unused]] T* insert(const T* pos, std::size_t count, const T& value)
+    {
+        return __insert_impl(pos, count, value);
+    }
+
+private:
+    T* __erase_impl(const T* first, const T* last)
+    {
+        T* it = const_cast<T*>(last);
+
+        T* new_it = const_cast<T*>(first);
+        while (it < _data_end)
+        {
+            *(new_it++) = std::move(*(it++));
+        }
+
+        T* new_end = new_it;
+
+        while (new_it < _data_end)
+        {
+            (new_it++)->~T();
+        }
+
+        _data_end = new_end;
+
+        return const_cast<T*>(last);
+    }
+
+public:
+    [[maybe_unused]] T* erase(T* pos)
+    {
+        return __erase_impl(pos, pos + 1);
+    }
+
+    [[maybe_unused]] T* erase(const T* pos)
+    {
+        return __erase_impl(pos, pos + 1);
+    }
+
+    [[maybe_unused]] T* erase(T* first, T* last)
+    {
+        return __erase_impl(first, last);
+    }
+
+    [[maybe_unused]] T* erase(const T* first, const T* last)
+    {
+        return __erase_impl(first, last);
+    }
+
+public:
+
+    void push_back(const T& value)
+    {
+        __insert_impl(_data_end, 1, value);
+    }
+
+    void push_back(T&& value)
+    {
+        __insert_impl(_data_end, 1, std::move(value));
+    }
+
+    template <class... Args>
+    T& emplace_back(Args&&... args);
+
+    void pop_back()
+    {
+        __erase_impl(_data_end - 1, _data_end);
+    }
+
+    void resize(std::size_t count)
+    {
+        this->resize(count, T());
+    }
+
+    void resize(std::size_t count, const T& value)
+    {
+        if (count == this->size()) return;
+
+        if (count < this->size())
+        {
+            __erase_impl(this->begin() + count, _data_end);
+        }
+        else
+        {
+            __insert_impl(_data_end, count - this->size(), value);
+        }
+    }
+
+    void swap(vector& other) noexcept
+    {
+        std::swap(_data_begin, other._data_begin);
+        std::swap(_data_end, other._data_end);
+        std::swap(_alloc_end, other._alloc_end);
+    }
 };
 
 } // namespace my_std
---
+++ tests/test_vector.cpp
@@ -0,0 +1,449 @@
+#include <catch2/catch_all.hpp>
+#include <iostream>
+
+#include <vector.hpp>
+
+TEST_CASE("vector bring-up", "[VECTOR]")
+{
+
+    SECTION("size only constructor")
+    {
+        constexpr std::size_t vec_size = 1;
+        my_std::vector<uint32_t> vec(vec_size);
+
+        REQUIRE(vec._data_begin != 0);
+        REQUIRE(vec[0] == uint32_t());
+        REQUIRE(vec.size() == vec_size);
+    }
+
+    SECTION("value constructor")
+    {
+        constexpr uint32_t init_value = 2;
+        auto vec_size = GENERATE(range(1, 5));
+
+        my_std::vector<uint32_t> vec(vec_size, init_value);
+
+        REQUIRE(vec.size() == vec_size);
+        for (std::size_t i = 0; i < vec_size; i++)
+        {
+            REQUIRE(vec.at(i) == init_value);
+        }
+    }
+
+    SECTION("empty vector")
+    {
+        constexpr std::size_t vec_size = 0;
+        my_std::vector<uint32_t> vec(vec_size);
+
+        REQUIRE(vec._data_begin == nullptr);
+        REQUIRE(vec.empty());
+    }
+    
+    SECTION("basic reserve")
+    {
+        constexpr std::size_t vec_size = 16;
+        my_std::vector<uint32_t> vec;
+        vec.reserve(vec_size);
+
+        SUCCEED("no double free / segfaults :)");
+    }
+
+    SECTION("reserve with existing vector")
+    {
+        constexpr uint32_t init_value = 2;
+        constexpr std::size_t vec_size = 16;
+        constexpr std::size_t new_size = 32;
+
+        my_std::vector<uint32_t> vec(vec_size, init_value);
+        vec.reserve(new_size);
+
+        SUCCEED("no double free / segfaults :)");
+    }
+
+    SECTION("shrink_to_fit")
+    {
+        constexpr uint32_t init_value = 0xdeadbeef;
+        constexpr std::size_t vec_size = 16;
+        constexpr std::size_t new_size = 32;
+
+        my_std::vector<uint32_t> vec(vec_size, init_value);
+        vec.reserve(new_size);
+
+        vec.shrink_to_fit();
+
+        REQUIRE(vec.size() == vec_size);
+        REQUIRE(vec.capacity() == vec_size);
+    }
+
+    SECTION("clear")
+    {
+        constexpr uint32_t init_value = 0xdeadbeef;
+        constexpr std::size_t vec_size = 16;
+
+        my_std::vector<uint32_t> vec(vec_size, init_value);
+        vec.clear();
+
+        REQUIRE(vec.size() == 0);
+        REQUIRE(vec.capacity() == vec_size);
+    }
+}
+
+TEST_CASE("single copy insertion bring-up", "[VECTOR]")
+{
+    constexpr std::size_t vec_size   = 8;
+    my_std::vector<uint32_t> vec(vec_size);
+    constexpr uint32_t insert_val = 0xff00ff00;
+
+    SECTION("copy insert without resize")
+    {
+        constexpr std::size_t alloc_size = 16;
+        vec.reserve(alloc_size);
+
+        SECTION("copy insert single element at front")
+        {
+            uint32_t* insert_pos = vec.begin();
+            uint32_t* new_pos = vec.insert(insert_pos, insert_val);
+
+            REQUIRE(vec.front() == insert_val);
+            REQUIRE(insert_pos == new_pos);
+            REQUIRE(vec.begin() == insert_pos); // check begin hasn't changed
+            REQUIRE(*(new_pos) == insert_val);
+        }
+        SECTION("copy insert single element in middle")
+        {
+            uint32_t* insert_pos = vec.begin() + (vec_size / 2);
+            uint32_t* new_pos = vec.insert(insert_pos, insert_val);
+
+            REQUIRE(vec[vec_size / 2] == insert_val);
+            REQUIRE(insert_pos == new_pos);
+            REQUIRE(*(new_pos) == insert_val);
+        }
+        SECTION("copy insert single element at back")
+        {
+            uint32_t* insert_pos = vec.end();
+            uint32_t* new_pos = vec.insert(insert_pos, insert_val);
+
+            REQUIRE(vec.back() == insert_val);
+            REQUIRE(insert_pos == new_pos);
+            REQUIRE(*(new_pos) == insert_val);
+        }
+    }
+
+    SECTION("copy insert while resize")
+    {
+        SECTION("copy insert single element at front")
+        {
+            uint32_t* insert_pos = vec.begin();
+            uint32_t* new_pos = vec.insert(insert_pos, insert_val);
+
+            REQUIRE(vec.front() == insert_val);
+            REQUIRE(vec.begin() != insert_pos); // this is a valid check because when we allocate we haven't deallocated the old mem yet so the insert_pos is not available
+            REQUIRE(vec.begin() == new_pos);
+        }
+
+        SECTION("copy insert single element in middle")
+        {
+            uint32_t* insert_pos = vec.begin() + vec_size / 2;
+            uint32_t* new_pos = vec.insert(insert_pos, insert_val);
+
+            REQUIRE(vec[vec_size / 2] == insert_val);
+            REQUIRE(*(new_pos) == insert_val);
+        }
+
+        SECTION("copy insert single element at back")
+        {
+            uint32_t* insert_pos = vec.end();
+            uint32_t* new_pos = vec.insert(insert_pos, insert_val);
+
+            REQUIRE(vec.back() == insert_val);
+            REQUIRE(*(new_pos) == insert_val);
+        }
+    }
+}
+
+TEST_CASE("single move insertion bring-up", "[VECTOR]")
+{
+    struct move_only_t
+    {
+        uint32_t* ptr = nullptr;
+        move_only_t() { ptr = nullptr; }
+        move_only_t(uint32_t val)
+        {
+            ptr = new uint32_t;
+            *ptr = val;
+        }
+        ~move_only_t()
+        {
+            if (ptr != nullptr)
+            {
+                delete ptr;
+            }
+        }
+
+        // delete copy constructor;
+        move_only_t(const move_only_t&) = delete;
+
+        move_only_t(move_only_t&& rhs)
+        {
+            if (ptr != nullptr)
+            {
+                delete ptr;
+            }
+            ptr = rhs.ptr;
+            rhs.ptr = nullptr;
+        }
+
+        move_only_t& operator=(move_only_t&& rhs)
+        {
+            if (ptr != nullptr)
+            {
+                delete ptr;
+            }
+            ptr = rhs.ptr;
+            rhs.ptr = nullptr;
+            return *this;
+        }
+    };
+
+    constexpr std::size_t vec_size = 8;
+    my_std::vector<move_only_t> vec(vec_size);
+    constexpr uint32_t setup_val = 1;
+
+    for (auto& e : vec)
+    {
+        e.ptr = new uint32_t(setup_val);
+    }
+
+    SECTION("move insert without resize")
+    {
+        constexpr std::size_t alloc_size = 16;
+        vec.reserve(alloc_size);
+
+        SECTION("move insert at front")
+        {
+            move_only_t insert_obj(2);
+            uint32_t* insert_ptr_val = insert_obj.ptr;
+            move_only_t* begin = vec.begin();
+            vec.insert(vec.begin(), std::move(insert_obj));
+
+            REQUIRE(vec.begin() == begin);
+            REQUIRE(vec.front().ptr == insert_ptr_val);
+        }
+    }
+
+    SECTION("move insert with resize")
+    {
+        SECTION("move insert at front")
+        {
+            move_only_t insert_obj(2);
+            uint32_t* insert_ptr_val = insert_obj.ptr;
+            vec.insert(vec.begin(), std::move(insert_obj));
+
+            REQUIRE(vec.front().ptr == insert_ptr_val);
+        }
+    }
+}
+
+TEST_CASE("multi insertion bring-up", "[VECTOR]")
+{
+    constexpr std::size_t vec_size   = 8;
+    my_std::vector<uint32_t> vec(vec_size);
+    constexpr uint32_t insert_val = 0xffeeddcc;
+
+    SECTION("multi insertion without resize")
+    {
+        constexpr std::size_t alloc_size = 16;
+        vec.reserve(alloc_size);
+
+        SECTION("insert 2 at front")
+        {
+            constexpr std::size_t insert_size = 2;
+            vec.insert(vec.begin(), insert_size, insert_val);
+
+            REQUIRE(vec.front() == insert_val);
+            REQUIRE(vec[1] == insert_val);
+        }
+
+        SECTION("insert 2 at back")
+        {
+            constexpr std::size_t insert_size = 2;
+            uint32_t* return_pos = vec.insert(vec.end(), insert_size, insert_val);
+
+            REQUIRE(vec.back() == insert_val);
+            REQUIRE(*return_pos == insert_val);
+            REQUIRE(*(return_pos+1) == insert_val);
+        }
+    }
+
+    SECTION("multi insertion with resize")
+    {
+        SECTION("insert 2 at front")
+        {
+            constexpr std::size_t insert_size = 2;
+            uint32_t* insert_pos = vec.begin();
+            uint32_t* return_pos = vec.insert(insert_pos, insert_size, insert_val);
+
+            REQUIRE(vec.front() == insert_val);
+            REQUIRE(vec.begin() != insert_pos);
+            REQUIRE(return_pos == vec.begin());
+            REQUIRE(vec[1] == insert_val);
+        }
+
+        SECTION("insert 2 at back")
+        {
+            constexpr std::size_t insert_size = 2;
+            uint32_t* insert_pos = vec.end();
+            uint32_t* return_pos = vec.insert(insert_pos, insert_size, insert_val);
+
+            REQUIRE(vec.back() == insert_val);
+            REQUIRE(insert_pos != vec.end());
+            REQUIRE(*return_pos == insert_val);
+            REQUIRE(*(return_pos+1) == insert_val);
+        }
+    }
+}
+
+TEST_CASE("erase bring-up", "VECTOR")
+{
+    constexpr std::size_t vec_size = 16;
+    my_std::vector<uint32_t> vec(vec_size);
+    std::vector<uint32_t> expected_final_vector = { 1, 2, 3, 4, 6, 7, 8, 9, 12, 13, 14, 15};
+
+    for (int i = 0; i < vec.size(); i++)
+    {
+        vec[i] = i;
+    }
+
+    vec.erase(vec.begin());
+    REQUIRE(vec.size() == vec_size - 1);
+    REQUIRE(vec.front() == 1);
+    
+    vec.erase(vec.begin() + 4);
+    REQUIRE(vec.size() == vec_size - 2);
+    REQUIRE(vec[3] == 4);
+    REQUIRE(vec[4] == 6);
+
+    vec.erase(vec.begin()+8, vec.begin()+10);
+    REQUIRE(vec.size() == vec_size - 4);
+    REQUIRE(vec[7] == 9);
+    REQUIRE(vec[8] == 12);
+    REQUIRE(vec[9] == 13);
+
+
+    REQUIRE(vec.size() == expected_final_vector.size());
+    for (int i = 0; i < vec.size(); i++)
+    {
+        REQUIRE(vec[i] == expected_final_vector[i]);
+    }
+}
+
+TEST_CASE("push_back bring-up", "[VECTOR]")
+{
+    constexpr std::size_t vec_size = 8;
+    my_std::vector<uint32_t> vec(vec_size);
+
+    SECTION("no resize")
+    {
+        constexpr std::size_t alloc_size = 16;
+        vec.reserve(alloc_size);
+
+        vec.push_back(1);
+
+        REQUIRE(vec.size() == vec_size+1);
+        REQUIRE(vec.back() == 1);
+    }
+
+    SECTION("with resizing")
+    {
+        vec.push_back(1);
+        REQUIRE(vec.size() == vec_size+1);
+        REQUIRE(vec.capacity() > vec_size);
+        REQUIRE(vec.back() == 1);
+    }
+}
+
+TEST_CASE("pop_back bring-up", "[VECTOR]")
+{
+    constexpr std::size_t vec_size = 8;
+    my_std::vector<uint32_t> vec(vec_size);
+
+    for (int i = 0; i < vec.size(); i++)
+    {
+        vec[i] = i;
+    }
+
+    REQUIRE(vec.back() == vec.size()-1);
+
+    vec.pop_back();
+
+    REQUIRE(vec.size() == vec_size - 1);
+    REQUIRE(vec.back() == vec.size() - 1);
+}
+
+TEST_CASE("resize bring-up", "[VECTOR]")
+{
+    constexpr std::size_t vec_size = 8;
+    using test_t = uint32_t;
+    my_std::vector<test_t> vec(vec_size);
+
+    SECTION("resize to same size")
+    {
+        vec.resize(vec_size);
+        REQUIRE(vec.size() == vec_size);
+    }
+
+    SECTION("resize to bigger")
+    {
+        constexpr std::size_t resize_factor = 2;
+        SECTION("without value")
+        {
+            vec.resize(vec_size * resize_factor);
+
+            REQUIRE(vec.size() == vec_size * resize_factor);
+            REQUIRE(vec.back() == test_t());
+        }
+
+        SECTION("with value")
+        {
+            constexpr test_t val = 0x11223344;
+            vec.resize(vec_size * resize_factor, val);
+
+            REQUIRE(vec.size() == vec_size * resize_factor);
+            REQUIRE(vec.back() == val);
+        }
+    }
+
+    SECTION("resize to smaller")
+    {
+        constexpr std::size_t resize_divisor = 2;
+        vec.resize(vec_size / resize_divisor);
+
+        REQUIRE(vec.size() == vec_size / resize_divisor);
+    }
+}
+
+
+TEST_CASE("swap bring-up", "[VECTOR]")
+{
+    using test_t = uint32_t;
+    constexpr std::size_t vec_size_1 = 2;
+    constexpr test_t init_value_1 = 0x11111111;
+
+    constexpr std::size_t vec_size_2 = 4;
+    constexpr test_t init_value_2 = 0x22222222;
+
+
+    my_std::vector<test_t> vec1(vec_size_1, init_value_1);
+    my_std::vector<test_t> vec2(vec_size_2, init_value_2);
+
+    vec1.swap(vec2);
+
+    REQUIRE(vec1.size() == vec_size_2);
+    REQUIRE(vec2.size() == vec_size_1);
+
+    REQUIRE(vec1.front() == init_value_2);
+    REQUIRE(vec2.front() == init_value_1);
+
+    REQUIRE(vec1[1] == init_value_2);
+    REQUIRE(vec2[1] == init_value_1);
+}
